60254bce85acf336fd60499c4dfee7db13b75474
LeroyM084 <78626550+LeroyM084@users.noreply.github.com>
2025-01-17 23:57:47 +0100

    Bouton save, save URL et password dans une fonction

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,24 +1,30 @@
-function generatePassword() {
+let generatedPassword = 'TETS'; // Variable globale pour stocker le mot de passe
+
+// Fonction asynchrone pour générer le mot de passe
+async function generatePassword() {
     const length = 25; // Définir une longueur ou demander une entrée utilisateur
-    fetch(`http://localhost:5000/generate-password?length=${length}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Erreur lors de l'appel à l'API");
-            }
-            return response.json(); // Convertit la réponse en JSON
-        })
-        .then(data => {
-            console.log("Password généré : ", data); // Vérification dans la console
-            document.getElementById("result").innerHTML = data.password;
-        })
-        .catch(error => {
-            console.error("Erreur :", error);
-            document.getElementById("result").innerHTML = "Erreur lors de la génération du mot de passe.";
-        });
+    try {
+        const response = await fetch(`http://localhost:5000/generate-password?length=${length}`);
+
+        if (!response.ok) {
+            throw new Error("Erreur lors de l'appel à l'API");
+        }
+
+        const data = await response.json(); // Convertit la réponse en JSON
+        console.log("Password généré : ", data); // Vérification dans la console
+        document.getElementById("result").innerHTML = data.password;
+
+        // Stocker le mot de passe dans la variable globale
+        generatedPassword = data.password;
+
+        return data;
+    } catch (error) {
+        console.error("Erreur :", error);
+        document.getElementById("result").innerHTML = "Erreur lors de la génération du mot de passe.";
+    }
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const resultDiv = document.getElementById('result');
     const copyButton = document.getElementById('copy');
     const copyIconPath = "/ChromeExtension/assets/imgs/copy_icon.png";
@@ -46,6 +52,18 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 
-    // Appeler la fonction de génération de mot de passe au chargement
-    generatePassword();
+    // Attendre que le mot de passe soit généré avant d'appeler saveURLAndPassword
+    await generatePassword();
+    saveURLAndPassword();
 });
+
+// Utilisation de la variable globale dans une autre fonction
+async function saveURLAndPassword() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        let currentURL = tabs[0].url; // Permet de sauvegarder l'URL actuelle.
+        console.log("URL actuelle : ", currentURL);
+    });
+    let currentPassword = generatePassword();
+
+
+}
